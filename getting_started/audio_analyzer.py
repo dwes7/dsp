6aad86e344e55a39b66d00373260b91a7183830a
Westley Barragan <dwestley.1992@gmail.com>
2018-11-12 17:46:18 -0800

    started real-time audio analyzer

---
+++ getting_started/audio_analyzer.py
@@ -0,0 +1,37 @@
+import pyaudio
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+CHUNK = 1024 * 4
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK
+)
+
+fig, ax = plt.subplots()
+x = np.arange(0, 2*CHUNK, 2)
+line,= ax.plot(x,np.random.rand(CHUNK))
+plt.show(block=False)
+ax.set_ylim(-255,255 )
+ax.set_xlim(0, CHUNK)
+while True:
+    
+    data = stream.read(CHUNK)
+    data_int16 = struct.unpack(str(CHUNK)+'h', data)
+    line.set_ydata(data_int16)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+    
